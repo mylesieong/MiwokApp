18bef047eeb6029f2ce044ae38c93052e071ab47
Myles Ieong <Myles Ieong>
2016-11-09 00:15:38 +0800

    ListView sample

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -54,14 +54,12 @@ public class NumbersActivity extends AppCompatActivity {
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_numbers.xml layout file.
-        //ListView listView = (ListView) findViewById(R.id.list);
-        GridView gridView = (GridView)findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        //listView.setAdapter(itemsAdapter);
-        gridView.setAdapter(itemsAdapter);
+        listView.setAdapter(itemsAdapter);
     }
 }
