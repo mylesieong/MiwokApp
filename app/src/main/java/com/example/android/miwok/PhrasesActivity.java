c1ad4588375666651bded91ffe5ec4dd60cc877c
Myles Ieong <Myles Ieong>
2016-11-12 23:55:40 +0800

    add phase part

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -24,5 +27,22 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?","minto wuksus"));
+        words.add(new Word("What is your name?","tinnə oyaase'nə"));
+        words.add(new Word("My name is...","oyaaset..."));
+        words.add(new Word("How are you feeling?","michəksəs?"));
+        words.add(new Word("I’m feeling good.","kuchi achit"));
+        words.add(new Word("Are you coming?","əənəs'aa?"));
+        words.add(new Word("Yes, I’m coming.","həə’ əənəm"));
+        words.add(new Word("I’m coming.","əənəm"));
+        words.add(new Word("Let’s go.","yoowutis"));
+        words.add(new Word("Come here.","ənni'nem"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
