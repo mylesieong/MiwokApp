1aabe26b36f61bd009ad4f3f87a334b8e27dacb7
Myles Ieong <Myles Ieong>
2016-11-13 15:35:43 +0800

    Add image related method in Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,22 +5,60 @@ package com.example.android.miwok;
  */
 
 public class Word {
-    private String mTextMiwok="";
-    private String mTextEng="";
-    public Word(String miwok, String english){
-        this.mTextEng = english==null?"":english;
-        this.mTextMiwok = miwok==null?"":miwok;
+
+    private String mTextMiwok = "";
+    private String mTextEng = "";
+    private int mImageResourceId = Word.NO_IMAGE_RESOURCE;
+
+    private final static int NO_IMAGE_RESOURCE = -1;
+
+    /**
+     * Contructor to receive 2 input of text,assume that there is no image resource
+     *
+     * @param miwok
+     * @param english
+     */
+    public Word(String miwok, String english) {
+        this.mTextEng = english == null ? "" : english;
+        this.mTextMiwok = miwok == null ? "" : miwok;
+        this.mImageResourceId = Word.NO_IMAGE_RESOURCE;
     }
-    public void setTextMiwok(String s){
+
+    /**
+     * Contructor to receive 2 input of text,assume that there is no image resource
+     *
+     * @param miwok
+     * @param english
+     */
+    public Word(String miwok, String english, int imageId) {
+        this.mTextEng = english == null ? "" : english;
+        this.mTextMiwok = miwok == null ? "" : miwok;
+        this.mImageResourceId = imageId > 0 ? imageId : Word.NO_IMAGE_RESOURCE;
+    }
+
+    public void setTextMiwok(String s) {
         this.mTextMiwok = s;
     }
-    public String getTextMiwok(){
-        return this.mTextMiwok==null?"":this.mTextMiwok;
+
+    public String getTextMiwok() {
+        return this.mTextMiwok == null ? "" : this.mTextMiwok;
     }
-    public void setTextEng(String s){
+
+    public void setTextEng(String s) {
         this.mTextEng = s;
     }
-    public String getTextEng(){
-        return this.mTextEng==null?"":this.mTextEng;
+
+    public String getTextEng() {
+        return this.mTextEng == null ? "" : this.mTextEng;
     }
+
+    /**
+     * Return these is no image resource for this word object
+     * p.s. no image resource setter so far beside the contructor
+     * @return
+     */
+    public boolean hasImageResource() {
+        return this.mImageResourceId != Word.NO_IMAGE_RESOURCE;
+    }
+
 }
