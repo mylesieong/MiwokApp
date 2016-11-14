55e0902cc4bdf1aed3ac8bb23b16ab39a7e60c1d
Myles Ieong <Myles Ieong>
2016-11-14 23:19:58 +0800

    Add bg color to all activities

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.content.Context;
 import android.os.WorkSource;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +20,14 @@ import java.util.List;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, List<Word> objects) {
+    /**
+     * Color resource for the Text fields' background color setting
+     */
+    private int mColorResourceId;
+
+    public WordAdapter(Context context, List<Word> objects, int colorResourceId) {
         super(context, 0, objects);
+        this.mColorResourceId = colorResourceId>0?colorResourceId:0;
     }
 
     @Override
@@ -59,6 +66,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
         }else{
             iconView.setVisibility(View.GONE);
         }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_list);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         /** Return the whole list item layout (containing 2 TextViews and an ImageView)
                  *   so that it can be shown in the ListView
